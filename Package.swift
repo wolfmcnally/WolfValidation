2386f2765f62149974fa718fc2c716017440f2fa
Wolf McNally <wolf@wolfmcnally.com>
2019-09-27 14:24:53 -0700

    Added support for Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "WolfValidation",
+    platforms: [
+        .iOS(.v12), .macOS(.v10_14), .tvOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "WolfValidation",
+            targets: ["WolfValidation"]),
+        ],
+    dependencies: [
+        .package(url: "https://github.com/wolfmcnally/WolfCore", from: "5.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfLocale", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfNIO", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "WolfValidation",
+            dependencies: [
+                "WolfCore",
+                "WolfLocale",
+                "WolfNIO",
+            ])
+        ]
+)
--- WolfValidation.podspec
+++ WolfValidation.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name             = 'WolfValidation'
-    s.version          = '3.0.1'
+    s.version          = '3.0.2'
     s.summary          = 'Framework for validating user-entered data including phone numbers and email addresses.'
 
     s.homepage         = 'https://github.com/wolfmcnally/WolfValidation'
